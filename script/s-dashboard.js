fdec2b989015ea9e80a6f06345af5c5960625de8
AnujaKarlekar <77532750+AnujaKarlekar@users.noreply.github.com>
2021-07-28 11:57:57 +0530

    Add files via upload

---
+++ script/s-dashboard.js
@@ -0,0 +1,11 @@
+function load_edit_profile() {
+    document.getElementById("content").style.display="block";
+    document.getElementById("frame1").style.display="none";
+    document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="edit_profile.html" ></object>';
+}
+
+function load_medical() {
+    document.getElementById("content").style.display="block";
+    document.getElementById("frame1").style.display="none";
+    document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="shoplist.html" ></object>';
+}
