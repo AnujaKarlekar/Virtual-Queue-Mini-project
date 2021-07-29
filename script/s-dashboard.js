a3da8053c39d9088a32370f1c534abc92b382cbb
AnujaKarlekar <77532750+AnujaKarlekar@users.noreply.github.com>
2021-07-29 16:49:15 +0530

    UI updates

--- script/s-dashboard.js
+++ script/s-dashboard.js
@@ -4,8 +4,14 @@ function load_edit_profile() {
     document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="edit_profile.html" ></object>';
 }
 
-function load_medical() {
+function load_edit_shopdetails() {
     document.getElementById("content").style.display="block";
     document.getElementById("frame1").style.display="none";
-    document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="shoplist.html" ></object>';
+    document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="edit_shop_details.html" ></object>';
+}
+
+function load_manage_timeslot() {
+    document.getElementById("content").style.display="block";
+    document.getElementById("frame1").style.display="none";
+    document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="ShopSlots.html" ></object>';
 }
