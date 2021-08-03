84a92dc442abe08b007c60ecdf3a2e280115ea3a
Apoorv Katkar <apoorv422@gmail.com>
2021-08-03 13:01:22 +0530

    customer side integration done

--- script/c-dashboard.js
+++ script/c-dashboard.js
@@ -2,6 +2,46 @@ function load_edit_profile() {
     document.getElementById("content").style.display="block";
     document.getElementById("frame1").style.display="none";
     document.getElementById("content").innerHTML='<object width="100%" height="100%" type="text/html" data="edit_profile.html" ></object>';
+    //window.location = "../edit_profile.html"
+    let cookie = document.cookie
+    var cookies = cookie.split(';')
+    var username = cookies[1].split('=')[1]
+}
+function logout() {
+    //console.log("here")
+    document.cookie = "UserName=; expiress= Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "UserID=; expiress= Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "RoleID=; expiress= Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    window.location = "../index.html"
+}
+function read_cookie(cname) {
+    var cookies = document.cookie.split(';')
+    for(var i in cookies) {
+        var value = cookies[i].split('=')
+        //console.log(value)
+        if(cname == value[0].trim())
+            return value[1].toString()
+    }
+    return null
+}
+
+function cancel(button) {
+    var token = button.id
+    var reply = window.confirm("Do yo want cancel this appointment?\ntoken = " + token)
+    // document.getElementById(token).onclick = function() {
+    if(reply){
+        var url = "https://localhost:44300/deleteslot/" +  token
+        let request = new XMLHttpRequest()
+            request.open("DELETE", url);
+            request.send();
+            request.onload = () => {
+                if(request.status === 200){
+                    alert("Appointment deleted!!")
+                }
+                else
+                    alert("Could not delete the apoointment")
+            }
+    }
 }
 
 function load_medical() {
@@ -61,7 +101,7 @@ function load_time_slot(shop) {
     document.getElementById("content").style.display="block";
     document.getElementById("frame1").style.display="none";
     var id = shop.id
-    console.log(id) //
+    //console.log(id) //
     var shopName = document.getElementById(id).innerText;
     document.getElementById("content").innerHTML = '<div id="main-panel"> <!link rel="stylesheet" type="text/css" href="asd.css" />'
     document.getElementById("content").innerHTML = '<h2 class="name"><center><p id = "shop-name"></p></center><br></h2></div>'//<h3 class="name">Opening hours: 9:00 am to 4:00pm</h3>'
@@ -75,11 +115,11 @@ function load_time_slot(shop) {
     request.onload = () => {
         if(request.status === 200){
             var rows = JSON.parse(request.response)
-            console.log(rows)
+            //console.log(rows)
             html = '<div id = "main-panel"> <div class="row"><div class="col-25"><center><h2>Available Slots</h2><br><h3>Select Time Slot:</h3></center></div><center>'
             html += '<div class="col-75"><select style="text-align: center;" id="Available Slots" name="Available Slots">'
             for(var i = 0; i < rows.length; i++) {
-                html += '<option value=' + rows[i].startTime.split('T')[0] + ' ' + rows[i].startTime.split('T')[1]
+                html += '<option value=' + rows[i].startTime.split('T')[0] + 'T' + rows[i].startTime.split('T')[1]
                 html += 'to'  + rows[i].endTime.split('T')[1] + '>'
                 html += rows[i].startTime.split('T')[0] + ' ' 
                 html +=rows[i].startTime.split('T')[1] + ' to'  + rows[i].endTime.split('T')[1] +'</option>'
@@ -87,7 +127,7 @@ function load_time_slot(shop) {
             }
             html += '</select></div></center></div>'
             html += '<div><p align="center"><button type="button" style="background-color:rgb(255, 174, 0);'
-            html += 'margin-left:auto;margin-right:auto;display:block;margin-top:10%;margin-bottom:0%"> Book Slot </button>'
+            html += 'margin-left:auto;margin-right:auto;display:block;margin-top:10%;margin-bottom:0%", onclick="book_slot(this)", id=' + id + '> Book Slot </button>'
             html += '</p></button></p></div>'
             document.getElementById("content").innerHTML += html
         } 
@@ -96,6 +136,33 @@ function load_time_slot(shop) {
         }
     }
             
+}
+
+function book_slot(shop) {
+    var reply = window.confirm("Do you want to book this slot?\n Time:" + document.getElementById("Available Slots").value)
+    var shopid = shop.id.split("-")[1]
+    //console.log(shopid)
+    if(reply) {
+        var apmnt_details = {
+            customerID: read_cookie('UserID'),
+            shopId: shopid,
+            startTime: document.getElementById("Available Slots").value.split('to')[0],
+            endTime:   document.getElementById("Available Slots").value.split('T')[0] + 'T' + 
+            document.getElementById("Available Slots").value.split('to')[1]
+        }
+        var url = "https://localhost:44300/bookslot/"
+        let request = new XMLHttpRequest()
+        request.open("PUT", url);
+        request.setRequestHeader("Content-type", "application/json")
+        request.send(JSON.stringify(apmnt_details));
+        request.onload = () => {
+            if(request.status === 200)
+                alert("Appointment Booked")
+            else
+                alert("Couldn't book the slot")
+        }
+                
+    }
 }
 //fot sorting
 var menu = document.querySelector(".dropdown-content");
@@ -141,7 +208,7 @@ menu.addEventListener("click", function(e){
                 
                 }
                 html += '</table>';
-                console.log(html)
+                //console.log(html)
                 document.getElementById('content').innerHTML = html;
             
             
@@ -152,6 +219,91 @@ menu.addEventListener("click", function(e){
     }
 });
 
+document.addEventListener("DOMContentLoaded", function() {   
+    let cookie = document.cookie
+    var cookies = cookie.split(';')
+    console.log(document.cookie)
+    var username = read_cookie('UserName')//cookies[1].split('=')[1]
+    var userid = read_cookie('UserID')//cookies[2].split('=')[1]
+    document.getElementById('username').innerText = username
+    if(username == "") {
+        window.location = "../index.html"
+    }
+    var url = 'https://localhost:44300/gethistory/' + userid;
+    //console.log(url)
+    let request = new XMLHttpRequest()
+    request.open("GET", url);
+    request.send();
+    request.onload = () => {
+        if(request.status === 200){
+            var rows = JSON.parse(request.response)
+            //console.log(rows
+            var html = '<center><h2>Bookings History</h2><br></br>'
+            html += '<table id="current">';
+            html += '<tr>';
+            var hed = ["Sr.No.", "Shop", "Place", "Date", "Time", "Token", "Status"]
+            for( var j in hed ) {
+                html += '<th>' + hed[j] + '</th>';
+            }
+            html += '</tr>';
+            html += '</tr>';
+            for( var i = 0; i < rows.length; i++) {
+                html += '<tr>';
+                html += '<td>' + (i + 1) + '</td>';
+                var url1 = 'https://localhost:44300/searchbyid/' + rows[i].shopId;
+                
+                let request1 = new XMLHttpRequest()
+                request1.open("GET", url1);
+                
+                var shopname = ""
+                var address = ""
+                html += '<td id =' + i + '>' + shopname    + '</td>';
+                html += '<td id = addr'+ i + '>' + address + '</td>';
+                var count = 0
+                request1.onload = () => {
+                if(request1.status === 200){
+                    var shop = JSON.parse(request1.response)
+                    //console.log(shop
+                    
+                    shopname = shop[0].shopName
+                    address = shop[0].adress
+                    //return (shopname, address)
+                    document.getElementById(count).innerText = shopname
+                    document.getElementById('addr' + count++).innerText = address
+                }
+                }
+                request1.send();
+                
 
+                
+                if(rows[i].startTime) {
+                    html += '<td>' + rows[i].startTime.split('T')[0] + '</td>';
+                    html += '<td>' + rows[i].startTime.split('T')[1] + '</td>';
+                }
+                else {
+                    html += '<td>' + "placeholder" + '</td>';
+                    html += '<td>' + "placeholder" + '</td>'
+                }
+                html += '<td>' + rows[i].token + '</td>';
+                if(rows[i].isCompleted) {
+                    html += '<td>' + 'Completed' + '</td>'
+                       
+                }
+                else {
+                    html += '<td>' + 'Not Completed' + '</td>'
+                    html += '<td><button class="btn" class="button button1" onclick="cancel(this)" id =' 
+                    html += rows[i].token + '>Cancel</button></td>'
+                }
+                html += '</tr>';
+            
+            }
+            html += '</table></center>';
+            document.getElementById("content").style.display="block";
+            document.getElementById("frame1").style.display="none";
+            document.getElementById("content").innerHTML = html
+        }
+    }
+
+}, false)
 
     
---
+++ script/login.js
@@ -0,0 +1,38 @@
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    let request = new XMLHttpRequest()
+    request.open("GET","https://localhost:44300/isauthenticated/" + '?UserName=' + username + '&Passwd=' + password);
+    request.send();
+    request.onload = () => {
+        if(request.status === 200){
+                var date = new Date()
+                date.setTime(date.getTime()+(30*60*1000))
+                var expires = "; expires="+date.toUTCString();
+
+                var rows = JSON.parse(request.response)
+
+                if(rows["item2"]){
+                    document.cookie = "UserName=" + username + expires + "; path=/";
+                    document.cookie = "UserID=" + rows["item1"] + expires + "; path=/"
+                    document.cookie = "RoleID=" + rows["item3"] + expires + "; path=/"
+                if(rows["item3"] == 1){
+                    window.location = "../admin-dashboard.html" 
+                }
+                if(rows["item3"] == 2){
+                    window.location = "../customer-dashboard.html"
+                }
+                if(rows["item3"] == 3){
+                    window.location = "../shop-dashboard.html"
+                }
+                }
+                else{
+                alert("Not a registered user.")
+                }
+        } 
+        else {
+                console.log("Page not found")
+        }
+    }
+
+}
